fe68564997bd7fc21eff95cfb5d0e2bd4ce424a2
Vinnath Satharasinghe <vinnathninura@gmail.com>
2023-08-15 12:24:53 +0530

    Create Server & Config (ex7)

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
-console.log("let's ssgo")
+// this is server file - which means this is the starting point of the development !!
+
+import express from "express";
+import cors from "cors"; // communicate with backend application (backend specificly work with one front-end application and restric all other reqs !!)
+import dotenv from "dotenv"; // use for access environmental variables (Not Public urls configaration)
+//dotenv.config(); - 1st way
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT || "8090"  // check PORT or(||) use 8090 PORT
+
+app.use(cors()); // defalt access all requests
+app.use(express.json({limit: "20mb"}));
+
+app.get("/", (req, res, next) =>{
+    res.send("<h2> jsu go men")
+})
+
+app.listen(PORT, () => {
+
+    console.log(('🛰️  Server is running now on PORT ')+(PORT));
+});
+
+
+
+ 
